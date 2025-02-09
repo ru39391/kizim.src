27550d31b075123382e57a633f2d57f5e12612fe
ru39391 <dev@39391.ru>
2025-02-10 02:59:44 +0300

    feature/стилизовать скроллер с видео

--- src/scripts/modules/slides.ts
+++ src/scripts/modules/slides.ts
@@ -2,22 +2,22 @@ import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 
 const initSlides = (sel: string) => {
-  const item = document.querySelector(sel);
+  const items = Array.from(document.querySelectorAll(sel));
 
-  if(!item) {
+  if(!items.length) {
     return;
   }
 
-  const slides = new Swiper(sel, {
+  const slides = items.map(() => new Swiper(sel, {
     slidesPerView: 'auto',
     loop: true,
     centeredSlides: true,
     modules: [Navigation],
     navigation: {
-      nextEl: `.swiper-button-next`,
-      prevEl: `.swiper-button-prev`,
+      nextEl: `${sel} .swiper-button-next`,
+      prevEl: `${sel} .swiper-button-prev`,
     },
-  });
+  }));
 
   return slides;
 }
